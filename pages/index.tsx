8df583b4dd41afa3ab9d5b924dee2ce965108542
Choi Sung Sik <css9596@naver.com>
2022-02-16 23:23:15 +0900

    Tailwind Practice Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-slate-400 py-20 px-10 grid gap-10">
+        <div className="bg-slate-400 py-20 px-20 grid gap-10">
             <div className="bg-white p-6 rounded-3xl shadow-2xl">
                 <span className="font-semibold text-3xl">Select Item</span>
                 <div className="flex justify-between my-2">
@@ -19,9 +19,32 @@ const Home: NextPage = () => {
                 </div>
                 <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">Checkout</div>
             </div>
-            <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
-            <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
-            <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+            <div className="bg-white overflow-hidden rounded-3xl shadow-2xl">
+                <div className="bg-blue-500 p-6">
+                    <span className="text-white text-2xl pb-14">Profile</span>
+                </div>
+                <div className="rounded-3xl p-6 bg-white relative -top-5">
+                    <div className="flex relative -top-16 items-end justify-between">
+                        <div className="flex flex-col items-center">
+                            <span className="text-sm text-gray-500">Orders</span>
+                            <span className="font-medium">340</span>
+                        </div>
+                        <div className="h-24 w-24 bg-red-400 rounded-full">
+
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <span className="text-sm text-gray-500">Spent</span>
+                            <span className="font-medium">$340</span>
+                        </div>
+                    </div>
+                    <div className="relative mt-5 flex flex-col items-center -mt-10 -mb-5">
+                        <span className="text-lg font-medium">Tony Molloy</span>
+                        <span className="text-sm text-gray-500">미국</span>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white p-6 rounded-3xl shadow-2xl"></div>
+            <div className="bg-white p-6 rounded-3xl shadow-2xl"></div>
         </div>
     );
 };
