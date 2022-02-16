87414b6d5a4f2ebb4d090af5928c63f15db02b17
Choi Sung Sik <css9596@naver.com>
2022-02-17 00:22:50 +0900

    Tailwind Practice Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import type {NextPage} from 'next';
 const Home: NextPage = () => {
     return (
         <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-            <div className="bg-white p-6 rounded-3xl shadow-2xl">
-                <span className="font-semibold text-3xl">Select Item</span>
+            <div className="bg-white p-6 rounded-3xl shadow-xl">
+                <span className="font-semibold text-2xl">Select Item</span>
                 <div className="flex justify-between my-2">
                     <span className="text-gray-500">Grey Chair</span>
                     <span className="font-semibold">$19</span>
@@ -17,7 +17,12 @@ const Home: NextPage = () => {
                     <span>Total</span>
                     <span className="font-semibold">$10</span>
                 </div>
-                <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">Checkout</div>
+                <button
+                    className="mt-5 bg-blue-500 text-white p-3
+                    text-center rounded-xl w-3/4 block mx-auto
+                    hover:bg-teal-500 hover:text-black
+                    active:bg-yellow-500 focus:text-red-500">Checkout
+                </button>
             </div>
             <div className="bg-white overflow-hidden rounded-3xl shadow-2xl">
                 <div className="bg-blue-500 p-6">
@@ -53,25 +58,31 @@ const Home: NextPage = () => {
                         <span className="shadow-xl p-2 rounded-md">❤️</span>
                     </div>
                 </div>
-                <div className="bg-zinc-400 h-72 mb-5" />
+                <div className="bg-zinc-400 h-72 mb-5"/>
                 <div className="flex flex-col text-xl">
                     <span className="font-medium">Swoon Lounge</span>
                     <span className="text-xs text-gray-500">Chair</span>
                     <div className="mt-3 mb-5 flex justify-between items-center">
-                        <div>
-                            <input type="radio"/>
-                            <input type="radio"/>
-                            <input type="radio"/>
+                        <div className="space-x-2">
+                            <button className="w-5 h-5 rounded-full bg-yellow-500"/>
+                            <button className="w-5 h-5 rounded-full bg-indigo-500"/>
+                            <button className="w-5 h-5 rounded-full bg-teal-500-500"/>
                         </div>
                         <div className="flex items-center space-x-5">
-                            <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">+</button>
+                            <button
+                                className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">+
+                            </button>
                             <span>1</span>
-                            <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-</button>
+                            <button
+                                className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">-
+                            </button>
                         </div>
                     </div>
                     <div className="flex justify-between items-center">
                         <span className="font-medium text-2xl">$450</span>
-                        <button className="bg-blue-500 py-2 px-8 text-xs text-center text-white rounded-lg">Add to cart</button>
+                        <button className="bg-blue-500 py-2 px-8 text-xs text-center text-white rounded-lg">Add to
+                            cart
+                        </button>
                     </div>
                 </div>
             </div>
