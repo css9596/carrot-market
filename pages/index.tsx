d3dfb04224ee59ecb48dcba9c4170f2bf5f51b7b
Choi Sung Sik <css9596@naver.com>
2022-02-19 00:43:57 +0900

    Tailwind Practice Four (Modifiers for Lists)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ const Home: NextPage = () => {
         <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
             <div className="bg-white p-6 rounded-3xl shadow-xl">
                 <span className="font-semibold text-2xl">Select Item</span>
-                <div className="flex justify-between my-2">
-                    <span className="text-gray-500">Grey Chair</span>
-                    <span className="font-semibold">$19</span>
-                </div>
-                <div className="flex justify-between">
-                    <span className="text-gray-500">Grey Chair</span>
-                    <span className="font-semibold">$19</span>
-                </div>
+                <ul>
+                    {[1,2,3,4,5].map(i => (
+                        <div key={i} className="flex justify-between my-2">
+                            <span className="text-gray-500">Grey Chair</span>
+                            <span className="font-semibold">$19</span>
+                        </div>
+                    ))}
+                {["a", "b", "c", ""].map((c,i) => <li className="bg-red-500 py-2 empty:hidden
+                " key={i}>{c}</li>)}
+                </ul>
                 <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
                     <span>Total</span>
                     <span className="font-semibold">$10</span>
@@ -84,7 +86,6 @@ const Home: NextPage = () => {
                     </div>
                 </div>
             </div>
-            <div className="bg-white p-6 rounded-3xl shadow-2xl"></div>
         </div>
     );
 };
