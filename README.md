69b7474444089e5447d85042a0cb5fc5fa5d2178
Choi Sung Sik <css9596@naver.com>
2022-02-19 20:29:52 +0900

    Tailwind Practice Five

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Carrot Market
 ## Skill Set
 ### Next.js, Tailwind, Prisma, PlanetScale and Cloudflare 를 사용해서 만든다.
+
+
+## Tailwind 동작방식
+과거(Tailwind 3.0 이전)에 Tailwind는 엄청 큰 CSS파일로 이루어져 있어 class를 중첩하여 사용할 수 없었고,
+개발이 완료되고 배포하기 전에 사용하지 않는 class 삭제처리(purging)를 했었다.
+이후 Tailwind 3.0 버전이 나오면서 Just-In-Time(JIT) 컴파일러가 도입이 되면서 class를 중첩하여 사용할 수 있게 되었다.
+*JIT Compiler?
+    내가 작성한 코드를 실시간으로 감시하면서 필요한 클래스를 생성하는 기능을 하는 Tailwind 컴파일러
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,100 @@
 import type {NextPage} from 'next';
+import {useState} from "react";
 
 const Home: NextPage = () => {
+    const [dark, setDark] = useState(false);
+    const isDark = () => {
+        setDark(!dark);
+    }
     return (
-      <div className="flex flex-col space-y-2 p-5">
-        {/*<details className="select-none open:text-white open:bg-indigo-400">
-            <summary className="select-none cursor-pointer">
-                What is my fav. food.
-            </summary>
-            <span className="selection:bg-indigo-500">
-                Kimchi
-            </span>
-        </details>*/}
-          {/*<ul className="list-decimal marker:text-teal-500">
-              <li>hi</li>
-              <li>hi</li>
-              <li>hi</li>
-              <li>hi</li>
-              <li>hi</li>
-              <li>hi</li>
-          </ul>*/}
-          <input type="file" className="file:cursor-pointer file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400" />
-      </div>
+        <div className={(dark ? "dark" : "")}>
+            <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+                <button className="" onClick={isDark}>{!dark? "다크모드" : "다크모드 해제"}</button>
+                <div className="bg-white dark:bg-black flex flex-col p-6 rounded-3xl shadow-xl">
+                    <span className="font-semibold text-2xl dark:text-white">Select Item</span>
+                    <div className="flex justify-between my-2">
+                        <span className="text-gray-50 dark:text-gray-100">Grey Chair</span>
+                        <span className="font-semibold dark:text-white">$19</span>
+                    </div>
+                    <div className="flex justify-between my-2">
+                        <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
+                        <span className="font-semibold dark:text-white">$19</span>
+                    </div>
+                    <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+                        <span>Total</span>
+                        <span className="font-semibold">$10</span>
+                    </div>
+                    <button
+                        className="mt-5 bg-blue-500 text-white p-3
+                        dark:bg-black dark:text-white dark:border-white dark:border
+                        text-center rounded-xl w-3/4 block mx-auto
+                        dark:hover:bg-white hover:bg-teal-500 hover:text-black
+                        active:bg-yellow-500 focus:text-red-500">Checkout
+                    </button>
+                </div>
+                <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
+                    <div className="bg-blue-500 p-6 pb-14 xl:pb-52">
+                        <span className="text-white text-2xl pb-14">Profile</span>
+                    </div>
+                    <div className="rounded-3xl p-6 bg-white relative -top-5">
+                        <div className="flex relative -top-16 items-end justify-between">
+                            <div className="flex flex-col items-center">
+                                <span className="text-sm text-gray-500">Orders</span>
+                                <span className="font-medium">340</span>
+                            </div>
+                            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-red-300 transition-colors">
+
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <span className="text-sm text-gray-500">Spent</span>
+                                <span className="font-medium">$340</span>
+                            </div>
+                        </div>
+                        <div className="relative mt-5 flex flex-col items-center -mt-10 -mb-5">
+                            <span className="text-lg font-medium">Tony Molloy</span>
+                            <span className="text-sm text-gray-500">미국</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-white p-6 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1">
+                <div className="flex mb-5 justify-between items-center">
+                    <span>⬅️</span>
+                    <div className="space-x-3">
+                        <span>⭐️ 4.9</span>
+                        <span className="shadow-xl p-2 rounded-md">💖</span>
+                    </div>
+                </div>
+                <div className="bg-zinc-400 h-72 mb-5" />
+                <div className="flex flex-col">
+                    <span className="font-medium text-xl">Swoon Lounge</span>
+                    <span className="text-xs text-gray-500">Chair</span>
+                    <div className="mt-3 mb-5 flex justify-between items-center">
+                        <div className="space-x-2">
+                            <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition" />
+                            <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition" />
+                            <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition" />
+                        </div>
+                        <div className="flex items-center space-x-5">
+                            <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                                -
+                            </button>
+                            <span>1</span>
+                            <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                                +
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <span className="font-medium text-2xl">$450</span>
+                        <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
     );
 };
 
-export default Home
+export default Home;
