cf5314a5e5291339d5d443b70a0e548a0e0654e9
Choi Sung Sik <css9596@naver.com>
2022-02-27 17:30:48 +0900

    업로드페이지 및 lable / input 연결

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -30,20 +30,25 @@ export default function Enter() {
                     </div>
                 </div>
                 <form className="flex flex-col mt-8">
-                    <label className="text-sm font-medium text-gray-700">
+                    <label htmlFor="input" className="text-sm font-medium text-gray-700">
                         {method === "email" ? "Email address" : null}
                         {method === "phone" ? "Phone number" : null}
                     </label>
                     <div className="mt-1">
-                        {method === "email" ? (<input type="email"
-                                                      className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                                                      required/>) : null}
+                        {method === "email" ? (
+                            <input
+                                    id="input"
+                                    type="email"
+                                    className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                                    required/>) : null}
                         {method === "phone" ? (
                             <div className="flex rounded-md shadow-sm">
                                 <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">+82</span>
-                                <input type="number"
-                                       className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                                       required/>
+                                <input
+                                    id="input"
+                                    type="number"
+                                    className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                                    required/>
                             </div>
                         ) : null}
                     </div>
@@ -71,7 +76,9 @@ export default function Enter() {
                                     d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"/>
                             </svg>
                         </button>
-                        <button className="flex justify-center items-center py-2 px-4 border  border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-500 hover:bg-gray-50">
+                        <button
+                            className="flex justify-center items-center py-2 px-4 border  border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-500 hover:bg-gray-50"
+                        >
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
