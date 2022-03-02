db4b3c91c9fcfd159398e9d3719a29a1d472e493
Choi Sung Sik <css9596@naver.com>
2022-03-02 23:09:20 +0900

    Component 변경 / 화면구성 완료

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,19 +1,27 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../../components/layout";
 
-const Index : NextPage = () => {
+const Chats: NextPage = () => {
     return (
-        <div className="py-10 divide-y-[1px]">
-                {[1, 2, 3, 4].map((_, i) => (
-                    <div key={i} className="flex px-4 cursor-pointer py-3 items-center space-x-3">
-                        <div className="w-12 h-12 rounded-full bg-slate-300" />
-                        <div>
-                            <p className="font-medium text-gray-700">Steve Jebs</p>
-                            <p className="text-sm text-gray-500">See you tomorrow in the corner at 2pm!</p>
-                        </div>
-                    </div>
+        <Layout hasTabBar title="채팅">
+            <div className="divide-y-[1px] ">
+                {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+                    <Link href={`/chats/${i}`} key={i}>
+                        <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+                            <div className="w-12 h-12 rounded-full bg-slate-300" />
+                            <div>
+                                <p className="text-gray-700">Steve Jebs</p>
+                                <p className="text-sm  text-gray-500">
+                                    See you tomorrow in the corner at 2pm!
+                                </p>
+                            </div>
+                        </a>
+                    </Link>
                 ))}
-        </div>
+            </div>
+        </Layout>
     );
 };
 
-export default Index;
+export default Chats;
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,84 +1,84 @@
-import {useState} from "react";
+import type { NextPage } from "next";
+import { useState } from "react";
+import Button from "../components/button";
+import Input from "../components/input";
+import { cls } from "../libs/utils";
 
-function cls(...classnames: string[]) {
-    return classnames.join(" ");
-}
-
-export default function Enter() {
+const Enter: NextPage = () => {
     const [method, setMethod] = useState<"email" | "phone">("email");
     const onEmailClick = () => setMethod("email");
     const onPhoneClick = () => setMethod("phone");
     return (
         <div className="mt-16 px-4">
-            <h3 className="text-3xl font-bold text-center">당근 마켓 클론코딩</h3>
-            <div className="mt-16">
+            <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+            <div className="mt-12">
                 <div className="flex flex-col items-center">
-                    <h5 className="text-sm text-gray-500 font-medium">Enter using</h5>
-                    <div className="grid border-b w-full mt-8 grid-cols-2 gap-16">
-                        <button className={cls(
-                            "pb-4 font-medium border-b-2",
-                            method === "email" ? " border-orange-500 text-orange-400"
-                                : "border-transparent text-gray-500"
-                        )} onClick={onEmailClick}>Email
+                    <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
+                    <div className="grid  border-b  w-full mt-8 grid-cols-2 ">
+                        <button
+                            className={cls(
+                                "pb-4 font-medium text-sm border-b-2",
+                                method === "email"
+                                    ? " border-orange-500 text-orange-400"
+                                    : "border-transparent hover:text-gray-400 text-gray-500"
+                            )}
+                            onClick={onEmailClick}
+                        >
+                            Email
                         </button>
-                        <button className={cls(
-                            "pb-4 font-medium border-b-2",
-                            method === "phone" ? " border-orange-500 text-orange-400"
-                                : "border-transparent text-gray-500"
-                        )} onClick={onPhoneClick}>Phone
+                        <button
+                            className={cls(
+                                "pb-4 font-medium text-sm border-b-2",
+                                method === "phone"
+                                    ? " border-orange-500 text-orange-400"
+                                    : "border-transparent hover:text-gray-400 text-gray-500"
+                            )}
+                            onClick={onPhoneClick}
+                        >
+                            Phone
                         </button>
                     </div>
                 </div>
-                <form className="flex flex-col mt-8">
-                    <label htmlFor="input" className="text-sm font-medium text-gray-700">
-                        {method === "email" ? "Email address" : null}
-                        {method === "phone" ? "Phone number" : null}
-                    </label>
-                    <div className="mt-1">
-                        {method === "email" ? (
-                            <input
-                                    id="input"
-                                    type="email"
-                                    className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                                    required/>) : null}
-                        {method === "phone" ? (
-                            <div className="flex rounded-md shadow-sm">
-                                <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">+82</span>
-                                <input
-                                    id="input"
-                                    type="number"
-                                    className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                                    required/>
-                            </div>
-                        ) : null}
-                    </div>
-                    <button className="mt-6 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-                        {method === "email" ? "Get login link" : null}
-                        {method === "phone" ? "Get one-time password" : null}
-                    </button>
+                <form className="flex flex-col mt-8 space-y-4">
+                    {method === "email" ? (
+                        <Input name="email" label="Email address" type="email" required />
+                    ) : null}
+                    {method === "phone" ? (
+                        <Input
+                            name="phone"
+                            label="Phone number"
+                            type="number"
+                            kind="phone"
+                            required
+                        />
+                    ) : null}
+                    {method === "email" ? <Button text={"Get login link"} /> : null}
+                    {method === "phone" ? (
+                        <Button text={"Get one-time password"} />
+                    ) : null}
                 </form>
+
                 <div className="mt-8">
                     <div className="relative">
-                        <div className="absolute w-full border-t border-gray-300"/>
-                        <div className="relative -top-3 text-center">
-                            <span className="bg-white px-2 text-sm text-gray-500">Or enter with</span>
+                        <div className="absolute w-full border-t border-gray-300" />
+                        <div className="relative -top-3 text-center ">
+              <span className="bg-white px-2 text-sm text-gray-500">
+                Or enter with
+              </span>
                         </div>
                     </div>
                     <div className="grid grid-cols-2 mt-2 gap-3">
-                        <button className="flex justify-center items-center py-2 px-4 border  border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-500 hover:bg-gray-50">
+                        <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                             >
-                                <path
-                                    d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"/>
+                                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                             </svg>
                         </button>
-                        <button
-                            className="flex justify-center items-center py-2 px-4 border  border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-500 hover:bg-gray-50"
-                        >
+                        <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -97,4 +97,5 @@ export default function Enter() {
             </div>
         </div>
     );
-}
+};
+export default Enter;
